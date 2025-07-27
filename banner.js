e88de89c3a2e39ea8a391d9d63d3f9d08801a2d0
tkwkrhcn <tkwkrhcn11@gmail.com>
2025-07-27 13:36:09 +0800

    Update banner.js

--- banner.js
+++ banner.js
@@ -116,10 +116,10 @@ document.head.appendChild(style);
 
 // --- 큐브 배너 1 ---
 const cubeBanners = [
-  { url: "https://t.me/betwizptcs/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/d16c063c-5f85-40ea-1486-10329cc7b400/public", alt: "벳위즈" },
-  { url: "https://t.me/ask1004/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/a54390cd-4825-45eb-97b2-9f7bd8985b00/public", alt: "아하" },
-  { url: "https://t.me/betwizptcs/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/d16c063c-5f85-40ea-1486-10329cc7b400/public", alt: "벳위즈" },
-  { url: "https://t.me/MUL114/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/d0640ad4-71ce-4581-bc83-bc22d4dbaf00/public", alt: "물음표" }
+  { url: "https://riflerivercampground.com/bn-4/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/d16c063c-5f85-40ea-1486-10329cc7b400/public", alt: "벳위즈" },
+  { url: "https://riflerivercampground.com/bn-3/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/a54390cd-4825-45eb-97b2-9f7bd8985b00/public", alt: "아하" },
+  { url: "https://riflerivercampground.com/bn-4/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/d16c063c-5f85-40ea-1486-10329cc7b400/public", alt: "벳위즈" },
+  { url: "https://riflerivercampground.com/bn-2/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/d0640ad4-71ce-4581-bc83-bc22d4dbaf00/public", alt: "물음표" }
 ];
 function renderCubeBanner() {
   const stage = document.createElement('div');
@@ -150,10 +150,10 @@ renderCubeBanner();
 
 // --- 큐브 배너 2 ---
 const cubeBanners2 = [
-  { url: "https://t.me/kr1xbetpartner/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/d85ee81d-6726-4002-50fa-939f283f3500/public", alt: "1xbet-1" },
-  { url: "https://t.me/krmelbet/", img: "https://imagedelivery.net/qv2gIkABq4Cpu3cwbEEy-g/53395395-aa41-4dae-71bf-7cd7a1473300/public", alt: "melbet-2" },
-  { url: "https://t.me/kr1xbetpartner/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/d85ee81d-6726-4002-50fa-939f283f3500/public", alt: "1xbet-2" },
-  { url: "https://t.me/krmelbet/", img: "https://imagedelivery.net/qv2gIkABq4Cpu3cwbEEy-g/53395395-aa41-4dae-71bf-7cd7a1473300/public", alt: "melbet-3" }
+  { url: "https://riflerivercampground.com/bn-5/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/d85ee81d-6726-4002-50fa-939f283f3500/public", alt: "1xbet-1" },
+  { url: "https://riflerivercampground.com/bn-6/", img: "https://imagedelivery.net/qv2gIkABq4Cpu3cwbEEy-g/53395395-aa41-4dae-71bf-7cd7a1473300/public", alt: "melbet-2" },
+  { url: "https://riflerivercampground.com/bn-5/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/d85ee81d-6726-4002-50fa-939f283f3500/public", alt: "1xbet-2" },
+  { url: "https://riflerivercampground.com/bn-6/", img: "https://imagedelivery.net/qv2gIkABq4Cpu3cwbEEy-g/53395395-aa41-4dae-71bf-7cd7a1473300/public", alt: "melbet-3" }
 ];
 function renderSecondCubeBanner() {
   const stage = document.createElement('div');
@@ -184,14 +184,14 @@ renderSecondCubeBanner();
 
 // --- 일반배너 ---
 const shuffledBanners = [
-  { url: "https://t.me/betwizptcs/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/fdec73b4-ebd8-4449-4ca1-cf11f276a100/public", alt: "벳위즈" },
-  { url: "https://t.me/MUL114/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/d11759ca-6c22-4b22-3ae0-3ed298893500/public", alt: "물음표" },
-  { url: "https://t.me/ask1004/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/13fc4195-2db7-4ecc-6317-792330765800/public", alt: "아하" },
-  { url: "https://t.me/kr1xbetpartner/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/3c899412-c689-4f4c-11bc-a7c1944f1e00/public", alt: "1xbet" },
-  { url: "https://t.me/krmelbet/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/e2e9e4b7-dcd3-43e4-c082-ece47d34f100/public", alt: "멜벳" }
+  { url: "https://riflerivercampground.com/bn-4/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/fdec73b4-ebd8-4449-4ca1-cf11f276a100/public", alt: "벳위즈" },
+  { url: "https://riflerivercampground.com/bn-2/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/d11759ca-6c22-4b22-3ae0-3ed298893500/public", alt: "물음표" },
+  { url: "https://riflerivercampground.com/bn-3/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/13fc4195-2db7-4ecc-6317-792330765800/public", alt: "아하" },
+  { url: "https://riflerivercampground.com/bn-5/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/3c899412-c689-4f4c-11bc-a7c1944f1e00/public", alt: "1xbet" },
+  { url: "https://riflerivercampground.com/bn-6/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/e2e9e4b7-dcd3-43e4-c082-ece47d34f100/public", alt: "멜벳" }
 ];
 const fixedBanners = [
-  { url: "https://t.me/oncs114/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/ad9b6afd-90a7-4fd1-82b0-d898534e7e00/public", alt: "총판모집" }
+  { url: "https://riflerivercampground.com/bn-1/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/ad9b6afd-90a7-4fd1-82b0-d898534e7e00/public", alt: "총판모집" }
 ];
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
